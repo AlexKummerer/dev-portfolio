656ea66344538d242eae38fa52590e269bcf3c9e
alexkummerer <alexander@thomas-kummerer.de>
2026-02-24 22:19:22 +0100

    feat: generate shared components (navbar, theme-toggle, lang-switch, project-card)

---
+++ src/app/components/lang-switch/lang-switch.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lang-switch',
+  imports: [],
+  templateUrl: './lang-switch.html',
+  styleUrl: './lang-switch.scss',
+})
+export class LangSwitch {
+
+}
