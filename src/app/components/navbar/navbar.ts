a1d54fb301d93aea3dd3aa1b09c2ee63724aa944
alexkummerer <alexander@thomas-kummerer.de>
2026-02-25 21:42:41 +0100

    feat: add navbar with scroll blur effect and navigation links

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, afterNextRender, signal } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ToolbarModule, NgClass],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  scrolled = signal(false);
 
+  constructor() {
+    afterNextRender(() => {
+      window.addEventListener('scroll', () => {
+        this.scrolled.set(window.scrollY > 50);
+      });
+    });
+  }
 }
